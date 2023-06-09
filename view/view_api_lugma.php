8d42ac87cda804124bd97c8ead19ff57f961af36
Juan <juan.paez@api-ux.com>
2023-06-09 15:37:06 -0500

    hot-fix view_repos

--- view/view_api_lugma.php
+++ view/view_api_lugma.php
@@ -1,7 +1,15 @@
 <?php
 require_once 'layout/header.php';
 require_once 'layout/form_api_lugma.php';
+
 require_once 'modal/modal_view_schedules.php';
+require_once 'layout/table_view_schedule.php';
+require_once 'modal/modal_insert_repo_loged.php';
+require_once 'modal/modal_view_repo_loged.php';
+require_once 'modal/modal_profile.php';
+require_once 'modal/modal_edit_profile.php';
+require_once 'modal/modal_change_password.php';
+
 require_once 'modal/modal_view_personal_tasks.php';
 require_once 'modal/modal_insert_task.php';
 
@@ -16,9 +24,7 @@ require_once 'modal/modal_member_groups.php';
 require_once 'modal/modal_created_groups.php';
 require_once 'modal/modal_public_groups.php';
 
-require_once 'modal/modal_profile.php';
 require_once 'modal/modal_edit_profile.php';
 require_once 'modal/modal_change_password.php';
 require_once 'layout/table_view_schedule.php';
-
 ?>
